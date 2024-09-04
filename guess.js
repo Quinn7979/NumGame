9844b286d8924746dcf39a1ce50955e3085a9ef7
crystal <crysbonnie@gmail.com>
2024-09-04 13:29:31 -0600

    Update guess.js

--- guess.js
+++ guess.js
@@ -8,7 +8,7 @@ let numGuesses = 1;
 while (enteredNumber != randomNumber) {
 	if (enteredNumber == randomNumber) {
 		console.log(`You Are Right! ${randomNumber} It Only Took You ${numGuesses} Times!`);
-	} else if (enteredNumber > randomNumber) {
+	} else if ((enteredNumber) => randomNumber) {
 		console.log("That is to High");
 	} else if (enteredNumber < randomNumber) {
 		console.log("That is to Low");
