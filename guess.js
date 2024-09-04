91d729b0ec91121e0e4eb6656048c5a2e417ac10
crystal <crysbonnie@gmail.com>
2024-09-04 12:42:50 -0600

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,16 +1,16 @@
 // Do not remove this line
 const prompt = require("syncprompt");
-const number = Math.ceil(Math.random() * 20);
+const randomNumber = Math.ceil(Math.random() * 20);
 
 let enteredNumber = prompt("Guess a number from 1 to 20: ");
 let numGuesses = 1;
 
-while (enteredNumber != number) {
-	if (enteredNumber == number) {
+while (enteredNumber != randomNumber) {
+	if (enteredNumber == randomNumber) {
 		console.log("You Are Right!");
-	} else if (enteredNumber > number) {
+	} else if (enteredNumber > randomNumber) {
 		console.log("That is to High");
-	} else if (enteredNumber < number) {
+	} else if (enteredNumber < randomNumber) {
 		console.log("That is to Low");
 	}
 	enteredNumber = prompt("Please Try Again: ");
